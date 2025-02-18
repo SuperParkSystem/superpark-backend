6ffc2967d3748c65fd542b543760654b7605064f
n3rdy <eaadithyaa@gmail.com>
2025-02-19 00:08:12 +0530

    Added parkingOwner auth middleware and routes

--- routes/auth_routes.ts
+++ routes/auth_routes.ts
@@ -1,18 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import * as driver from "../controllers/driver_controller"
+import * as parkingOwner from "../controllers/parkingOwner_controller"
 
 import * as auth from "../middleware/auth_middleware"
 
 const router = express.Router()
 
 router.put('/driver', driver.createPut)
-
 router.post('/driver/token', driver.createTokenPost)
-
 router.use('/driver/verify', auth.driverAuth)
-
 router.get('/driver/verify', driver.testToken)
+
+router.put('/parkingOwner', parkingOwner.createPut)
+router.post('/parkingOwner/token', driver.createTokenPost)
+router.use('/parkingOwner/verify', auth.parkingOwnerAuth)
+router.get('/parkingOwner/verify', parkingOwner.testToken)
 
 //router.put('/parkingOwner', (req, res) => {
 //    // Create a parking owner
